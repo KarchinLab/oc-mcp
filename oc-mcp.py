63cf92e43a16718a20ff17d16a2f77f608ac89ed
Kyle Moad <kyle.moad@gmail.com>
2025-10-16 13:15:38 -0400

    initial commit

---
+++ oc-mcp.py
@@ -0,0 +1,68 @@
+from typing import Any
+import httpx
+from mcp.server.fastmcp import FastMCP
+import requests
+import logging
+import json
+
+logging.basicConfig(
+    filename="tool.log",            # log file name
+    filemode="a",                  # append mode; use "w" to overwrite each run
+    level=logging.INFO,            # minimum severity to log
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+# Initialize FastMCP server
+mcp = FastMCP("open-cravat")
+
+# Constants
+OC_API_BASE = "https://run.opencravat.org"
+
+@mcp.tool()
+async def get_allele(chrom: str, pos: int, ref_base: str, alt_base: str) -> str:
+    try:
+        url = f"{OC_API_BASE}/api/annotate?chrom={chrom}&pos={pos}&ref_base={ref_base}&alt_base={alt_base}&annotators=clinvar,go,gnomad4"
+        logging.info(url)
+        response = requests.get(url)
+        logging.info(response)
+        data = response.json()
+        out = json.dumps(data)
+        logging.info(out)
+        return '\n'+out+'\n'
+    except Exception as e:
+        logging.exception(f'Error in get_variant: {e}')
+        raise
+
+@mcp.tool()
+async def get_rsid(rsid: str) -> str:
+    try:
+        url = f"{OC_API_BASE}/api/annotate?dbsnp={rsid}&annotators=clinvar,go,gnomad4"
+        logging.info(url)
+        response = requests.get(url)
+        logging.info(response)
+        data = response.json()
+        out = json.dumps(data)
+        logging.info(out)
+        return '\n'+out+'\n'
+    except Exception as e:
+        logging.exception(f'Error in get_variant: {e}')
+        raise
+
+@mcp.tool()
+async def get_caid(caid: str) -> str:
+    try:
+        url = f"{OC_API_BASE}/api/annotate?clingen={caid}&annotators=clinvar,go,gnomad4"
+        logging.info(url)
+        response = requests.get(url)
+        logging.info(response)
+        data = response.json()
+        out = json.dumps(data)
+        logging.info(out)
+        return '\n'+out+'\n'
+    except Exception as e:
+        logging.exception(f'Error in get_variant: {e}')
+        raise
+
+if __name__ == "__main__":
+    # Initialize and run the server
+    # mcp.run(transport='stdio')
+    mcp.run(transport='sse')

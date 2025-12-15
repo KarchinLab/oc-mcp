213d1df259c94272e7c70d17afd9413033c52d8c
Kyle Moad <kyle.moad@gmail.com>
2025-12-15 13:53:12 -0500

    add annotator selection

--- cloudflare/src/index.ts
+++ cloudflare/src/index.ts
@@ -1,6 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { McpAgent } from "agents/mcp";
 import { z } from "zod";
+import { parse, stringify } from 'yaml'
 
 // Define our MCP agent with tools
 export class MyMCP extends McpAgent {
@@ -12,17 +13,45 @@ export class MyMCP extends McpAgent {
 	async init() {
 
 		this.server.tool(
+			"list_annotators",
+			{
+				
+			},
+			async ({ }) => {
+				const manifestURL = 'https://store.opencravat.org/manifest.yml';
+				const response = await fetch(manifestURL);
+				const yamlText = await response.text();
+				const manifest = parse(yamlText);
+				const annotators = {};
+				for (let moduleName in manifest) {
+					let ocModule = manifest[moduleName];
+					if (ocModule.type === 'annotator') {
+						annotators[moduleName] = {
+							title: ocModule.title,
+							description: ocModule.description,
+							version: ocModule.latest_version,
+							tags: ocModule.tags,
+							dataSourceVersion: ocModule.datasource,
+						}
+					}
+				}
+				return { content: [{ type: "text", text: JSON.stringify(annotators, null, 2)}]}
+			}
+		);
+
+		this.server.tool(
 			"annotate_allele",
 			{
 				chromosome: z.string(),
 				position: z.number(),
 				referenceBase: z.string(),
 				alternateBase: z.string(),
-				
+				annotators: z.array(z.string()),
 			},
-			async ({ chromosome, position, referenceBase, alternateBase}) => {
+			async ({ chromosome, position, referenceBase, alternateBase, annotators, }) => {
 				const apiBase = 'https://run.opencravat.org';
-				const url = `${apiBase}/api/annotate?chrom=${chromosome}&pos=${position}&ref_base=${referenceBase}&alt_base=${alternateBase}&annotators=clinvar,go,gnomad4`;
+				const annotatorsArg = annotators.join(',');
+				const url = `${apiBase}/api/annotate?chrom=${chromosome}&pos=${position}&ref_base=${referenceBase}&alt_base=${alternateBase}&annotators=${annotatorsArg}`;
 				const response = await fetch(url);
 				const data = await response.json();
 				
@@ -34,11 +63,12 @@ export class MyMCP extends McpAgent {
 			"annotate_rsid",
 			{
 				rsid: z.string(),
-				
+				annotators: z.array(z.string()),
 			},
-			async ({ rsid}) => {
+			async ({ rsid, annotators, }) => {
 				const apiBase = 'https://run.opencravat.org';
-				const url = `${apiBase}/api/annotate?dbsnp=${rsid}&annotators=clinvar,go,gnomad4`;
+				const annotatorsArg = annotators.join(',');
+				const url = `${apiBase}/api/annotate?dbsnp=${rsid}&annotators=${annotatorsArg}`;
 				const response = await fetch(url);
 				const data = await response.json();
 				
@@ -50,11 +80,12 @@ export class MyMCP extends McpAgent {
 			"annotate_caid",
 			{
 				caid: z.string(),
-				
+				annotators: z.array(z.string()),
 			},
-			async ({ caid}) => {
+			async ({ caid, annotators, }) => {
 				const apiBase = 'https://run.opencravat.org';
-				const url = `${apiBase}/api/annotate?clingen=${caid}&annotators=clinvar,go,gnomad4`;
+				const annotatorsArg = annotators.join(',');
+				const url = `${apiBase}/api/annotate?clingen=${caid}&annotators=${annotatorsArg}`;
 				const response = await fetch(url);
 				const data = await response.json();
 				
